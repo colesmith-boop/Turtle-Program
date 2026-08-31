499c9b7e3f2cf5ff69fc2363c388e60cc86fbd86
colesmith-boop <cole_smith@paraparaumucollege.school.nz>
2026-08-31 14:41:53 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-print("Hello!")
+"""Custom Turtle Shape Creator."""
+
+import tkinter as tk 
+
+
+#Program settings
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 300
+CENTRE_X = CANVAS_WIDTH // 2
+CENTRE_Y = CANVAS HEIGHT // 2
+
+points = []
+
+
+def add_point(event):
+  """Record a point clicked by the user."""
+  x = event.x  - CENTRE_X
+  y = CENTRE_Y - event.y
+
+  points.append((x, y))
+
+  canvas.create_oval(
+      event.x - 3,
+      event.y - 3,
+      event.x + 3, 
+      event.y + 3,
+      fill="black"
+  )
+
+  if len(points) >= 2:
+    previous = points[-2]
+
+    canvas.create_line(
+            previous[0] + CENTRE_X,
+            CENTRE_Y - previous[1],
+            event.x,
+            event.y
+        )
+
+    coordinate_label.config(
+        text=f"Coordinates: {tuple(points)}"
+    )
+
+
+
+
+
+
+
+
+
+  
